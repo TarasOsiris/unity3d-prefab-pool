fda7e72465c22e6940ad7c391acf00260dc305a6
Taras Leskiv <leskiv.taras@gmail.com>
2013-09-08 15:15:20 +0300

    Comments added.

--- Simple Prefab Pool/Assets/Scripts/TestMyPool.cs
+++ Simple Prefab Pool/Assets/Scripts/TestMyPool.cs
@@ -6,12 +6,14 @@ public class TestMyPool : MonoBehaviour
 
     private void Start()
     {
-        int initialPoolSize = 15;
+        int initialPoolSize = 15; // number of prefabs that will be instantiated when pool is created
         int growth = 5; // number of new prefabs instantiated when the pool grows in size
         int maxPoolSize = int.MaxValue;
 
+        // Create pool of cubes containing instantiated cubePrefabs
         PrefabPool cubesPool = new PrefabPool(cubePrefab, initialPoolSize, growth, maxPoolSize);
 
+        // position, rotation and scale for prefab obtained from the pool
         Vector3 pos = new Vector3(2, 2, 2);
         Quaternion rotation = Quaternion.Euler(45.0f, 45.0f, 45.0f);
         Vector3 scale = new Vector3(2, 2, 2);
@@ -22,13 +24,15 @@ public class TestMyPool : MonoBehaviour
 
         for (int i = 0; i < myCubes.Length; i++)
         {
-            myCubes[i] = cubesPool.ObtainPrefab(pos*i, rotation, scale);
+            // obtained item becomes active the moment you retrieve it
+            myCubes[i] = cubesPool.ObtainPrefab(pos * i, rotation, scale); 
         }
 
         const int numberOfCubesToRecycle = 3;
         // Recycle prefabs that are not needed at the moment
         for (int i = 0; i < numberOfCubesToRecycle; i++)
         {
+            // recycled item becomes inactive the moment you recycle it
             cubesPool.RecyclePrefab(myCubes[i]);
         }
     }
