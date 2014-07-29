c273247cbe5cadd1349e7a53f58ebdb2941321bc
Taras Leskiv <leskiv.taras@gmail.com>
2014-07-29 17:45:15 +0300

    generic pool dev.

---
+++ Simple Prefab Pool/Assets/Scripts/Sample/MyCubePool.cs
@@ -0,0 +1,41 @@
+﻿using SimplePrefabPool;
+using UnityEngine;
+using System.Collections;
+
+public class MyCubePool : AbstractPrefabPool<MyCube>
+{
+    public MyCubePool(MyCube objectToCopy, GameObject parent) : base(objectToCopy, parent)
+    {
+    }
+
+    public MyCubePool(MyCube objectToCopy, GameObject parent, int initialSize) : base(objectToCopy, parent, initialSize)
+    {
+    }
+
+    public MyCubePool(MyCube objectToCopy, GameObject parent, int initialSize, int growth) : base(objectToCopy, parent, initialSize, growth)
+    {
+    }
+
+    public MyCubePool(MyCube objectToCopy, GameObject parent, int initialSize, int growth, int availableItemsMaximum) : base(objectToCopy, parent, initialSize, growth, availableItemsMaximum)
+    {
+    }
+
+    protected override void OnHandleAllocatePrefab(MyCube prefabInstance)
+    {
+        prefabInstance.gameObject.SetActive(true);
+        prefabInstance.renderer.material.color = Color.yellow;
+        prefabInstance.transform.position = new Vector3(-3, -3, -3);
+    }
+
+    protected override void OnHandleObtainPrefab(MyCube prefabInstance)
+    {
+        prefabInstance.gameObject.SetActive(true);
+        prefabInstance.renderer.material.color = Color.green;
+    }
+
+    protected override void OnHandleRecyclePrefab(MyCube prefabInstance)
+    {
+        prefabInstance.gameObject.SetActive(true);
+        prefabInstance.renderer.material.color = Color.red;
+    }
+}
